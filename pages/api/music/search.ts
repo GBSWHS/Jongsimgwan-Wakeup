ebc314c5791b13f71e4798ed12777eade15b59b3
noeulnight <noeulnight.mail@gmail.com>
2021-05-10 01:01:01 +0900

    fix: search api body

--- pages/api/music/search.ts
+++ pages/api/music/search.ts
@@ -9,6 +9,6 @@ export default function search (req: NextApiRequest, res: NextApiResponse) {
     .send({ title })
     .then((_res) => {
       if (!_res.body) return res.json({ success: false, msg: '검색 결과가 없거나 오류가 발생하였습니다.' })
-      res.send({ success: true, msg: _res.body })
+      res.send({ success: true, msg: _res.body.search })
     })
 }
