706a1c242ba5ef4cef15f2d3e661ee643c1cbe9b
noeulnight <noeulnight.mail@gmail.com>
2021-05-09 23:31:50 +0900

    fix: backend code

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ async function ResetData () {
   await db.from('voted').select('*').del()
   Melon(moment().format('MM/DD/YYYY'), { cutLine: 10 }).daily().then(chartData => {
     const chart = chartData.data[0]
-    superagent.post(`http://localhost:${process.env.PORT}/api/music/search`).send({ title: chart.artist + ' ' + chart.title }).then(async (data) => {
+    superagent.post(`http://localhost:${process.env.PORT}/search`).send({ title: chart.artist + ' ' + chart.title }).then(async (data) => {
       if (!data || !data.body.success) return console.log(data.body)
       const { title, duration, album, videoId, artists } = data.body.search
       await db.insert({ title, duration, album: JSON.stringify(album), artist: JSON.stringify(artists), id: videoId, uploadby: '0000' }).from('musicid').select('*')
