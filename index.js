9bd99ce4d7096854f61cef0d1ce17e2f9048a7c8
noeulnight <noeulnight.mail@gmail.com>
2021-05-09 23:36:48 +0900

    fix: Remote Special Characters

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { exec } = require('child_process')
 const express = require('express')
 const app = express()
 
+const regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"]/gi
+
 app.use(express.json())
 app.post('/search', (req, res) => {
-  exec('python ./main.py ' + req.body.title, (err, stdout, stderr) => {
+  exec('python ./main.py ' + req.body.title.replace(regExp, ''), (err, stdout, stderr) => {
     if (err) return res.json({ success: false, msg: err })
     return res.json({ success: true, msg: JSON.parse(stdout) })
   })
