78236607935c86f46b0bf9a996ac12fbc6959535
noeulnight <noeulnight.mail@gmail.com>
2021-05-09 23:27:33 +0900

    fix: youtube api

--- pages/api/music/search.ts
+++ pages/api/music/search.ts
@@ -1,13 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { exec } from 'child_process'
-import path from 'path'
+import { post } from 'superagent'
 
 export default function search (req: NextApiRequest, res: NextApiResponse) {
   const { title } = req.body
   if (!title) return res.json({ success: false, msg: '제목을 입력해주세요.' })
-  console.log(title)
-  exec(`${process.env.PYTHONEX} ${path.resolve()}/pages/api/music/index.py ${title}`, (err, out) => {
-    if (err || !JSON.parse(out)[0]) return res.send({ success: false, msg: '검색 결과가 없거나 오류가 발생했습니다.', err })
-    return res.send({ success: true, search: JSON.parse(out)[0] })
-  })
+  post('http://localhost:8010/search')
+    .set('content-type', 'application/json')
+    .send({ title })
+    .then((_res) => {
+      if (!_res.body) return res.json({ success: false, msg: '검색 결과가 없거나 오류가 발생하였습니다.' })
+      res.send({ success: true, msg: _res.body })
+    })
 }
